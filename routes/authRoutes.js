5c5fe704cb21ef700cba8923214f962bda41e9cf
shalinipriya10 <shalinipriya10.11@gmail.com>
2023-08-01 02:09:44 +0530

    updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,13 @@ app.get('/auth/google',passport.authenticate('google',{
     scope:['profile','email']
 
 }));
-app.get('/auth/google/callback',passport.authenticate('google'));
+app.get(
+'/auth/google/callback',
+passport.authenticate('google'),
+(req,res) => {
+    res.redirect('/surveys');
+}
+);
 app.get('/api/logout',(req,res)=>{
 req.logout();
 res.send(req.user);
